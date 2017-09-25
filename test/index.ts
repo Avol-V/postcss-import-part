0f8f30c5da3393491ecb98fc9ad2a35f1fc03451
Andrey Avol Volynkin <mail.avol@gmail.com>
2017-09-25 12:23:43 +0300

    Add tests

---
+++ test/index.ts
@@ -0,0 +1,75 @@
+import {expect} from 'chai';
+import {readFileSync} from 'fs';
+import 'mocha';
+import {resolve} from 'path';
+import * as postcss from 'postcss';
+import importPart, {
+	PluginOptions,
+} from '../src/index';
+
+describe(
+	'Import part',
+	() =>
+	{
+		it(
+			'should inject on top level',
+			() => check( 'simple' ),
+		);
+		
+		it(
+			'should inject on nested level',
+			() => check( 'nested' ),
+		);
+		
+		it(
+			'should import from web_modules',
+			() => check( 'modules' ),
+		);
+		
+		it(
+			'should allow custom resolve function',
+			() => check(
+				'custom-resolve',
+				{
+					resolve: ( path ) => (
+						/^~\//.test( path )
+						? resolve( __dirname, path.substr( 2 ) )
+						: path
+					),
+				},
+			),
+		);
+	},
+);
+
+/**
+ * Check PostCSS result with specific options.
+ * 
+ * @param options Plugin options.
+ * @param input Input CSS.
+ * @param output Expected output CSS.
+ */
+function check(
+	name: string,
+	options: Partial<PluginOptions> = {},
+): Promise<void>
+{
+	const sourceName = resolve( __dirname, `fixtures/${name}.pcss` );
+	const expectedName = resolve( __dirname, `fixtures/${name}.expected.pcss` );
+	
+	return postcss( [importPart( options )] )
+		.process(
+			readFileSync( sourceName, 'utf8' ),
+			{from: sourceName},
+		)
+		.then(
+			( result ) =>
+			{
+				expect(
+					result.css,
+				).to.equal(
+					readFileSync( expectedName, 'utf8' ),
+				);
+			},
+		);
+}
